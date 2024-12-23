9417936389d82276fa738d1cf9af77d283b97dd6
sanat918 <sanat918@gmail.com>
2024-12-23 09:52:06 +0530

    intialComment to namsteReact

---
+++ namasteReact.js
@@ -0,0 +1,20 @@
+// Chapter-1 (Inception)
+// -----------------------
+
+<html>
+  <head>
+    <title>Hello, World!</title>
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+  <body>
+    <h1 class="title">Hello World! </h1>
+    <p id="currentTime"></p>
+    <script>
+      let ele=document.createElement('div') newDiv.innerHTML = 'This is a new
+      div element created with JavaScript!'; document.body.appendChild(newDiv);
+    </script>
+  </body>
+</html>;
+
+
+
