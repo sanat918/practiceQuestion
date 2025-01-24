e0ce2fae02a4a5dd045ecd5b0cbdb38e3a354c17
sanat918 <sanat918@gmail.com>
2025-01-24 19:58:13 +0530

    added namasteSeries

---
+++ namasteFrontend.js
@@ -0,0 +1,57 @@
+// security headers 
+//1. x-Powered-By it can be removed from header as it's tells about the server it's express or any other
+//2. Referrel policy if i back or navigating from one page to another so in Referer i can see all params which i have passed in earlier request so it's not a good practice.
+//3. X-content-type-option X-content-type-option :"nosniff"
+//4. x-xss-protection 
+//5. x-content-types-option
+//6. HSTS
+
+//Client Storage security
+//1.Storing data on client storage
+// a. try ar server to store the data
+// b. Encrypt the data if storing on localStorage
+// c. token expiry
+//2. Authentication (like JWT or OAuth Token,  Token ssession(Expiry) , Multi-factor authentication)
+//3. Data Integrity 
+// a. checkSum--- Here i will store the checksum data in localStorage with my actual data which is needed to store in localstorage and before performing any task can comapre that checksum and the data is store in localstorage for use is equal or not. It's only to verify that someone has not performed any temper with data which is stored in localStorage which using the app.
+//4. Storage Limit (localStorage(5-10 MB),cookie((4-20 KB)), indexedDB(50- 100 MB approx.), sessionStorage(5-10 MB), cache (around 100 MB))
+//5. Session management
+
+
+//SecureCommunication security
+//1.Data Encryption(TSL)
+//2.Authentication(SSL & TSL)
+//3.Data Integripty(MAC)
+//4.Protection against Phishing
+//5. Data Privacy
+//6.Compliance with security standards
+//7.Trust and reputations
+//8.Search engine ranking
+//9.Potection against browser warnings
+//10.Faster website loading(HTTP2)
+
+
+/*DependencySecurity*/
+//1. Regular audits of dependencies(npm audits, npm update)
+//2. Enforcing auditing(npm set audit true) we can setup workflows in github using workflow that will keep track of these automatically in yaml file or github hooks.
+//3. COde and dependency monitor
+//4. Dependency locking(package-locking.json)
+//5. Security penetration testing using tools
+
+/*Compliance and Regulations*/
+// https://www.kiteworks.com/risk-compliance-glossary/regulatory-compliance-definition/#:~:text=Regulatory%20compliance%20is%20the%20process,regulations%20to%20maintain%20their%20operations.
+
+
+/* INputValidation and Sanitization */
+//1. Use framework or library(like react it's take care of these malicious activities)
+//2. Whitelisting validation
+//3. Regular Expression(to ensure the data value)
+//4. Escape user input
+//5. Parameterized URLs
+//6. Validate DataTypes
+//7. Length and Size check
+//8. Images & files
+//9. Add Client side validation as well as server
+//10.  Error Handling and Security Headers
+//11. Regualar updates and patch
+//12. 
--- namasteReact.js
+++ namasteReact.js
@@ -1,6 +1,7 @@
 // Chapter-1 (Inception)
 // -----------------------
 
+//with javascript
 <html>
   <head>
     <title>Hello, World!</title>
@@ -9,12 +10,46 @@
   <body>
     <h1 class="title">Hello World! </h1>
     <p id="currentTime"></p>
-    <script>
-      let ele=document.createElement('div') newDiv.innerHTML = 'This is a new
-      div element created with JavaScript!'; document.body.appendChild(newDiv);
-    </script>
   </body>
+  <script>
+    let nChild=document.createElement('h1')
+      nChild.innerHTML='Hello)))'
+      let dd=document.getElementById('currentTime')
+      dd.appendChild(nChild)
+    </script>
 </html>;
 
+//with react
+<html>
+  <head>
+    <title>Hello, World!</title>
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+  <body>
+   <div id="root">
+   <h1 class="title">Hello World! </h1>
+   <p id="currentTime"></p>
+   </div>
+  </body>
+  <script crossorigin src="https://unpkg.com/react@18/umd/react.development.js"></script>
+  <script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+  <script>
+   const heading=React.createElement("h1",{},"NamasteReact")
+   const root=ReactDom.createRoot(document.getElementById('root'))
+   root.render(root)
+  </script>
+</html>;
+
+//diff btwn async and defer
+
+// Parcel
+// Parsel is bundler. used to budle the prod=ject it also does image optimization, caching while development, clean our code , Dev and production build, compatilbe with older version of browser, hot module reloading, https in Dev, manages port number is execute two app in same time, we should put .parser-cache include in .gitignore, transitive dependency, TreeShaking- Removing unwanted Code, componetCompositions
+
+// JSX is not mandatory. ES6 is also not mandatory.
+
+// JSX only have one parenet if i didnt want the multiple  div i can simply use React.fragment. 
 
 
+
+
+5.  60 min
